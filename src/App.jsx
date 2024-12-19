f34a006dc5cb539a0faa82d58d2e02eafd3fd122
Kavidu Isura <144512406+KaviduIsura@users.noreply.github.com>
2024-12-19 12:04:34 +0530

    create template ✅💜

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProductCard from './components/productCard'
+import Testing from './components/testing'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Testing/>
+    </>
+  )
+}
+
+export default App
