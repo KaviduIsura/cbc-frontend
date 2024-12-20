cab32346cfa0cac36a9256ea4868269f96aed755
Kavidu Isura <144512406+KaviduIsura@users.noreply.github.com>
2024-12-20 14:30:05 +0530

    Do all routing parts 🛠️🛠️

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ProductCard from './components/productCard'
-import Testing from './components/testing'
 import LoginPage from './pages/login'
 import HomePage from './pages/home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SigninPage from './pages/signPage'
+import AdminHomePage from './pages/adminHomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <BrowserRouter>
       <Routes path ="/*">
         <Route path='/' element={<HomePage/>}></Route>
         <Route path='/login' element={<LoginPage/>}></Route>
-        <Route path='/*' element={<h1>404 Error</h1>}></Route>
+        <Route path='/signup' element={<SigninPage/>}></Route>
+        <Route path='/admin/*' element={<AdminHomePage/>}></Route>
+        <Route path='/*' element={<HomePage/>}></Route>
       </Routes>
       </BrowserRouter>
-    </>
+    </div>
   )
 }
 
