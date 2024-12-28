2961fa9eab8f122525160591074522048cf8435a
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-29 00:51:06 +0530

    Create useNavigate to rederect to the product page ⏮️

--- src/pages/admin/addProductForm.jsx
+++ src/pages/admin/addProductForm.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export default function AddProductForm() {
   const [productId, setProductId] = useState("");
@@ -11,6 +12,8 @@ export default function AddProductForm() {
   const [lastPrice, setLastPrice] = useState("");
   const [stock, setStock] = useState("");
   const [description, setDescription] = useState("");
+
+  const navigate = useNavigate();
 
   async function handleSubmit() {
     const altNames = alternativeNames.split(",");
@@ -32,8 +35,9 @@ export default function AddProductForm() {
           Authorization: "Bearer " + token,
         },
       });
+      navigate("/admin/products");
       toast.success("Product Add successfully");
-    } catch (err) {
+    } catch (error) {
       toast.error("Failed to add product");
     }
   }
