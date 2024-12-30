e1cfc57ad84291bd9195699486ab83d9c9100d05
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-30 12:56:25 +0530

    Remove Unwated routes and imports 🗑️✅

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SigninPage from "./pages/signPage";
 import AdminHomePage from "./pages/adminHomePage";
 import { Toaster } from "react-hot-toast";
-import FileUploadTest from "./pages/test";
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
           <Route path="/signup" element={<SigninPage />}></Route>
           <Route path="/admin/*" element={<AdminHomePage />}></Route>
           <Route path="/*" element={<HomePage />}></Route>
-          <Route path="/testing" element={<FileUploadTest />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
