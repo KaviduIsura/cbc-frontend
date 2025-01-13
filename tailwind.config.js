ed8258289f706d496e5fa930f4abc5fff8e07dcd
KaviduIsura <kaviduisura4567@gmail.com>
2025-01-13 17:01:33 +0530

    Define main color 🎨✅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f5f5f5",
+        secondary: "#545454",
+        accent: "#f2c977",
+      },
+    },
   },
   plugins: [],
-}
+};
