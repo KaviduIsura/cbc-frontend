bb9ded3c575f8f4dcb665e047ade687b8bedfef1
KaviduIsura <kaviduisura4567@gmail.com>
2025-02-12 01:17:15 +0530

    Redesign the login page 🎨✅

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -19,36 +19,50 @@ export default function LoginPage() {
         }
         toast.success("Login success");
         localStorage.setItem("token", res.data.token);
-        if ((res.data.user.type === "admin")) {
+        if (res.data.user.type === "admin") {
           window.location.href = "/admin";
         } else {
           window.location.href = "/";
         }
       });
   }
+
   return (
-    <div className="w-full h-screen bg-slate-400 flex items-center justify-center">
-      <div className="w-[500px] h-[500px] bg-slate-600 flex flex-col justify-center items-center ">
-        <img src="/logo.jpge.jpg" alt="" className="rounded-full w-[100px]" />
-        <span>Email</span>
+    <div
+      className="w-full h-screen bg-cover bg-center flex items-center justify-center "
+      style={{ backgroundImage: "url('/loginimg.jpg')" }} // Background Image
+    >
+      <div className="w-[400px] bg-[#ffffff] bg-opacity-50 p-6 rounded-lg shadow-lg flex flex-col items-center border-2 pt-0">
+        <div className="w-60 h-60 flex items-center justify-center mb-4">
+          <img
+            src="/logo1.png"
+            alt="Logo"
+            className="w-full h-full object-contain rounded-full "
+          />
+        </div>
+
+        <span className="w-full text-left text-black font-semibold pt-3 ">
+          Email
+        </span>
         <input
           type="text"
-          defaultValue={email}
-          onChange={(e) => {
-            //console.log(e.target.value);
-            setEmail(e.target.value);
-          }}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="p-2 m-2 mt-3 w-full border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#EDB065]"
         />
-        <span>Password</span>
+        <span className="w-full text-left text-black font-semibold">
+          Password
+        </span>
         <input
           type="password"
-          defaultValue={password}
-          onChange={(e) => {
-            //console.log(e.target.value);
-            setPassword(e.target.value);
-          }}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="p-2 m-2 mt-3 w-full border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#EDB065] "
         />
-        <button onClick={login} className="bg-white">
+        <button
+          onClick={login}
+          className="bg-[#EDB065] text-white px-4 py-2 rounded-md mt-4 hover:bg-[#964623] transition duration-300"
+        >
           Login
         </button>
       </div>
