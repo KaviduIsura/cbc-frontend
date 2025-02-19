17634aec81e3b770f930c1194c7b74d57390f4f2
KaviduICIEOS <kavidu@icieos.com>
2025-02-19 22:01:07 +0530

    Finish login page ✅✨

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
       className="w-full h-screen bg-cover bg-center flex items-center justify-center "
       style={{ backgroundImage: "url('/loginimg.jpg')" }} // Background Image
     >
-      <div className="w-[400px] bg-[#ffffff] bg-opacity-50 p-6 rounded-lg shadow-lg flex flex-col items-center border-2 pt-0">
+      <div className="relative w-[500px] h-[500px] bg-[#ffffff] bg-opacity-50 p-6 rounded-lg shadow-lg flex flex-col items-center border-2 pt-0">
         <div className="w-60 h-60 flex items-center justify-center mb-4">
           <img
             src="/logo1.png"
@@ -40,31 +40,41 @@ export default function LoginPage() {
             className="w-full h-full object-contain rounded-full "
           />
         </div>
-
-        <span className="w-full text-left text-black font-semibold pt-3 ">
-          Email
-        </span>
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="p-2 m-2 mt-3 w-full border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#EDB065]"
-        />
-        <span className="w-full text-left text-black font-semibold">
-          Password
-        </span>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="p-2 m-2 mt-3 w-full border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#EDB065] "
-        />
-        <button
-          onClick={login}
-          className="bg-[#EDB065] text-white px-4 py-2 rounded-md mt-4 hover:bg-[#964623] transition duration-300"
-        >
-          Login
-        </button>
+        <div className="absolute mt-40 p-8">
+          <span className="w-full text-left text-black font-semibold pt-3 pl-2 ">
+            Email
+          </span>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="p-2 m-2 mt-3 w-full border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#EDB065]"
+          />
+          <span className="w-full text-left text-black font-semibold pt-3 pl-2">
+            Password
+          </span>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="p-2 m-2 mt-3 w-full border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#EDB065] "
+          />
+          <button
+            onClick={login}
+            className="bg-[#EDB065] w-[150px] h-[40px] ml-36 text-white px-4 py-2 rounded-md mt-4 hover:bg-[#964623] transition duration-300"
+          >
+            Login
+          </button>
+          <p className="text-center text-sm text-gray-600 mt-3">
+            Already haven't an account?{" "}
+            <a
+              href="#"
+              className="font-medium text-[#EDB065] hover:text-[#964623]"
+            >
+              Sign up
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
