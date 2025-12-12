db32baafb078241ca2f48247094c9dbdf1e81613
KaviduIsura <kaviduisura4567@gmail.com>
2025-12-12 23:53:49 +0530

    change product card componat in product page

--- src/pages/home/product.jsx
+++ src/pages/home/product.jsx
@@ -18,7 +18,7 @@ import {
   ArrowRight,
   Loader
 } from "lucide-react";
-import ProductCard from "../../components/ProductCard";
+import ProductCard from "../../components/ProductCard.jsx";
 import axios from "axios";
 import toast from "react-hot-toast";
 import ProdImg from '../../assets/products.jpg'
